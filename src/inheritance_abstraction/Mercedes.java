4c144950d9c4ad823743c400d2f2d06980fb6f49
CrazyDevo <adam@cybertekschool.com>
2022-05-23 15:04:19 -0400

    Mercedes.java is arranged

--- src/inheritance_abstraction/Mercedes.java
+++ src/inheritance_abstraction/Mercedes.java
@@ -1,6 +1,6 @@
 package inheritance_abstraction;
 
-public class Mercedes extends Car{
+public class Mercedes extends Car implements LuxuryCar{ //Mercedes Is A Car
 
     public Mercedes(int year, String model, String color, double price) {
         super(year, "Mercedes", model, color, price);
@@ -10,4 +10,10 @@ public class Mercedes extends Car{
     public void start() {
         System.out.println("Start the engine with the start stop button");
     }
+
+    @Override
+    public void massage() {
+        System.out.println("Welcome to the Mercedes");
+    }
+
 }
